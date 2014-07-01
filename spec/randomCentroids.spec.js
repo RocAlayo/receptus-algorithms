73e8f63471f796aff46a489524ec8f0a3da0c420
Roc Alayo Arnabat <roc.alayo@gmail.com>
2014-07-01 13:04:22 +0200

    Changed to get random centroids from registers already exists not from min max values of every attribute

--- spec/randomCentroids.spec.js
+++ spec/randomCentroids.spec.js
@@ -3,31 +3,58 @@
  */
 "use strict";
 
-var randomCentroids = require("../lib/algorithms/clustering/positionOfCentroids/randomCentroids");
+var randomCentroids = require("../lib/algorithms/clustering/positionOfCentroids/randomCentroids"),
+    _ = require("lodash");
 
 /*global describe */
 /*global it */
 /*global expect */
 
 describe("randomCentroids", function () {
-  it("with data", function () {
-    var data = [
-        [24,2974,38040,704743],
-        [2,1592,87209,200333],
-        [70,8945,70320,969340],
-        [47,4700,10302,893011]
-        ],
-        centroids = randomCentroids(data, 2),
-        res  = centroids.length === 2 && centroids[0].length === 4;
+    it("Error - more clusters than data", function () {
+        var data = [
+            [24,2974,38040,704743],
+            [2,1592,87209,200333],
+            [70,8945,70320,969340],
+            [47,4700,10302,893011]
+        ];
 
-    res = res && centroids.every(function (el) {
-      var res = el[0] >= 2 && el[0] <= 70;
-      res = res && el[1] >= 1592 && el[1] <= 8945;
-      res = res && el[2] >= 10302 && el[2] <= 87209;
-      res = res && el[3] >= 200333 && el[3] <= 969340;
-      return res;
+        expect(function () {
+            randomCentroids(data, 5);
+        }).toThrow("Data is too small for the number of clusters");
     });
 
-    expect(res).toBeTruthy();
-  });
+    it("Use - more data than clusters", function () {
+        var data = [
+                [24,2974,38040,704743],
+                [2,1592,87209,200333],
+                [70,8945,70320,969340],
+                [47,4700,10302,893011]
+            ],
+            centroids = randomCentroids(data, 2),
+            res  = centroids.length === 2 && centroids[0].length === 4;
+
+        res = res && centroids.every(function (centroid) {
+            return data.indexOf(centroid) !== 1;
+        });
+
+        expect(res).toBeTruthy();
+    });
+
+    it("Use - same data than clusters", function () {
+        var data = [
+                [24,2974,38040,704743],
+                [2,1592,87209,200333],
+                [70,8945,70320,969340],
+                [47,4700,10302,893011]
+            ],
+            centroids = randomCentroids(data, 4),
+            res  = centroids.length === 4 && centroids[0].length === 4;
+
+        res = res && centroids.every(function (centroid) {
+            return data.indexOf(centroid) !== 1;
+        });
+
+        expect(res).toBeTruthy();
+    });
 });
